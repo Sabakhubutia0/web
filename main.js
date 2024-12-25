90af3e277d2b4fc6ef22a1f0154b99a4facd8d63
saba khubutia <bvb.xubutia@gmail.com>
2024-12-25 23:45:16 +0400

    pirveli seqcia gavaswore meore seqcia davamate da daviwye

---
+++ main.js
@@ -0,0 +1,15 @@
+// const images = ["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"];
+
+// let currentIndex = 0;
+
+// function changeBackground() {
+//   document.body.style.background = `url('${images[currentIndex]}') no-repeat center center fixed`;
+//   document.body.style.backgroundSize = "cover";
+
+//   currentIndex = (currentIndex + 1) % images.length;
+// }
+
+// // ცვლის სურათს ყოველ 2 წამში (2000 მილიწამი)
+// setInterval(changeBackground, 2000);
+
+// console.log("hello");
